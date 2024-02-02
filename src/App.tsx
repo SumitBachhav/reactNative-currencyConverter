5bd9d2b711424a1882d70a41f4206a2f1458b71a
SumitBachhav <sumitb2156@gmail.com>
2024-02-02 07:06:18 +0530

    adding logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,108 @@
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useState } from 'react';
 import {
   StyleSheet,
   View,
 } from 'react-native';
 
+//costants
+import { currencyByRupee } from './constants';
+//components
+import CurrencyBtn from './components/CurrencyBtn';
+
+import Snackbar from 'react-native-snackbar';
 
 function App(): React.JSX.Element {
+  const [inputValue, setInputValue] = useState('')
+  const [resultValue, setResultValue] = useState('')
+  const [targetCurrency, setTargetCurrency] = useState('')
 
+  const buttonPressed = (targetValue: Currency) => {
+    if (!inputValue)
+    {
+      return Snackbar.show({
+        text: "Enter a value to convert",
+        backgroundColor: "#EA7773",
+        textColor: "#000000"
+      })
+    }
 
+    const inputAmount = parseFloat(inputValue)
+    if(!isNaN(inputAmount)) {
+      const convertedValue = inputAmount * targetValue.value
+      const result = `${targetValue.symbol} ${convertedValue.toFixed(2)}`
+      setResultValue(result)
+      setTargetCurrency(targetValue.name)
+    }else{
+      return Snackbar.show({
+        text: "Not a valid number to convert",
+        backgroundColor: "#F4BE2C",
+        textColor: "#000000"
+      })
+    }
+  }
   return (
     <View></View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#515151',
+  },
+  topContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+  resultTxt: {
+    fontSize: 32,
+    color: '#000000',
+    fontWeight: '800',
+  },
+  rupee: {
+    marginRight: 8,
+
+    fontSize: 22,
+    color: '#000000',
+    fontWeight: '800',
+  },
+  rupeesContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  inputAmountField: {
+    height: 40,
+    width: 200,
+    padding: 8,
+    borderWidth: 1,
+    borderRadius: 4,
+    backgroundColor: '#FFFFFF',
+  },
+  bottomContainer: {
+    flex: 3,
+  },
+  button: {
+    flex: 1,
+
+    margin: 12,
+    height: 60,
+
+    borderRadius: 12,
+    backgroundColor: '#fff',
+    elevation: 2,
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+    shadowColor: '#333',
+    shadowOpacity: 0.1,
+    shadowRadius: 1,
+  },
+  selected: {
+    backgroundColor: '#ffeaa7',
+  },
 });
 
 export default App;
@@ -27,6 +114,10 @@ export default App;
 - create constants.ts for currency values object
 - create folder components
 - inside components create button component
+
+- using flatlist instead of scrollView
+- importing snackBar
+- 
 
 
 
