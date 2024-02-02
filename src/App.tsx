8f2ebd63c927f4b5959949cb16173f8fdb24e5d8
SumitBachhav <sumitb2156@gmail.com>
2024-02-02 05:46:50 +0530

    created index.d.ts file and button component and constants file holding objects

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  StyleSheet,
+  View,
+} from 'react-native';
+
+
+function App(): React.JSX.Element {
+
+
+  return (
+    <View></View>
+  );
+}
+
+const styles = StyleSheet.create({
+});
+
+export default App;
+
+/*
+- create src folder 
+- move app.ts to src
+- create index.d.ts to create interface(typescript) to provide structure and datatype of app
+- create constants.ts for currency values object
+- create folder components
+- inside components create button component
+
+
+
+
+*/
